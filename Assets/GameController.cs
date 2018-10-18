7e417dcc8c3f7df56acec9eadc66b2cae710683e
hanhsu032596 <43651161+hanhsu032596@users.noreply.github.com>
2018-10-18 23:39:23 +0800

    Initial commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public string randomOperator;
+    public int answer;
+    public int a;
+    public int b;
+    public GameObject question;
+    public GameObject option1;
+    public GameObject option2;
+    public GameObject option3;
+    public GameObject realAnswerOption;
+
+
+    void Start ()
+    {
+        GenerateQuestion();
+    }
+	
+	
+	void Update () {
+		
+	}
+
+    public void GenerateQuestion()
+    {
+        int a = Random.Range(0, 101);
+        int b = Random.Range(0, 101);
+        int operatorNumber = Random.Range(0, 3);
+
+        if (operatorNumber == 0)
+        {
+            randomOperator = "+";
+            answer = a + b;
+        }
+        else if(operatorNumber == 1)
+        {
+            randomOperator = "-";
+            answer = a - b;
+        }
+        else
+        {
+            randomOperator = "x";
+            answer = a * b;
+
+        }
+
+        int option = Random.Range(0, 3);
+
+        if (option == 0)
+        {
+            realAnswerOption = option1;
+            option1.GetComponent<TextMesh>().text = answer.ToString();
+            option2.GetComponent<TextMesh>().text = (answer + (Random.Range(0, 3) * 2 - 1)).ToString();
+            option3.GetComponent<TextMesh>().text = (answer + (Random.Range(-2, -4))).ToString();
+            option1.GetComponent<Option>().isCorrect = true;
+            option2.GetComponent<Option>().isCorrect = false;
+            option3.GetComponent<Option>().isCorrect = false;
+        }
+        else if (option == 1)
+        {
+            realAnswerOption = option2;
+            option1.GetComponent<TextMesh>().text = (answer + (Random.Range(0, 3) * 2 - 1)).ToString();
+            option2.GetComponent<TextMesh>().text = answer.ToString();
+            option3.GetComponent<TextMesh>().text = (answer + (Random.Range(-2, -4))).ToString();
+            option1.GetComponent<Option>().isCorrect = false;
+            option2.GetComponent<Option>().isCorrect = true;
+            option3.GetComponent<Option>().isCorrect = false;
+        }
+        else
+        {
+            realAnswerOption = option3;
+            option1.GetComponent<TextMesh>().text = (answer + (Random.Range(0, 3) * 2 - 1)).ToString();
+            option2.GetComponent<TextMesh>().text = (answer + (Random.Range(-2, -4))).ToString();
+            option3.GetComponent<TextMesh>().text = answer.ToString();
+            option1.GetComponent<Option>().isCorrect = false;
+            option2.GetComponent<Option>().isCorrect = false;
+            option3.GetComponent<Option>().isCorrect = true;
+        }
+
+        question.GetComponent<TextMesh>().text = a + " " + randomOperator + " " + b + " = ?";
+    }
+
+}
---
+++ Assets/Option.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option : MonoBehaviour {
+
+    public bool isCorrect;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        if (isCorrect)
+        {
+            GetComponent<TextMesh>().color = Color.green;
+        }
+
+        else
+        {
+            GetComponent<TextMesh>().color = Color.red;
+
+            if (GetComponent<GameController>().realAnswerOption = GetComponent<GameController>().option1)
+            {
+                GetComponent<TextMesh>().color = new Color(17, 79, 47, 255);
+            }
+            if (GetComponent<GameController>().realAnswerOption = GetComponent<GameController>().option2)
+            {
+                GetComponent<TextMesh>().color = new Color(17, 79, 47, 255);
+            }
+            if (GetComponent<GameController>().realAnswerOption = GetComponent<GameController>().option3)
+            {
+                GetComponent<TextMesh>().color = new Color(17, 79, 47, 255);
+            }
+        }
+
+    }
+
+}
